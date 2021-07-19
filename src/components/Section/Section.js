8e1b314da33affaa053d900e730727021c7d80ff
delongamateo <delongamateo@gmail.com>
2021-07-19 20:09:18 +0200

    making main component and styling

---
+++ src/components/Section/Section.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import styled from 'styled-components';
+
+function Section() {
+    return (
+      <Wrap>
+        <ItemText>
+          <h1>Model S</h1>
+          <p>Order Online for Touchles Delivery</p>
+        </ItemText>
+        <Buttons>
+          <ButtonGroup>
+            <LeftButton>Custom Order</LeftButton>
+            <RightButton>Existing Inventory</RightButton>
+          </ButtonGroup>
+          <DownArrow src="/images/down-arrow.svg" />
+        </Buttons>
+      </Wrap>
+    );
+}
+
+const Wrap = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-image: url("/images/model-s.jpg");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
+
+`;
+
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 30px;
+`;
+
+const LeftButton = styled.div`
+    background-color: rgb(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+    cursor: pointer;
+    margin: 10px;
+`;
+
+const RightButton = styled(LeftButton)`
+    background-color: white;
+    opacity: 0.65;
+    color: black;
+`;
+
+const DownArrow = styled.img`
+  height: 40px;
+  animation: animateDown infinite 1s;
+  overflow-x: hidden;
+`;
+
+const Buttons = styled.div`
+    text-align: center;
+`;
+
+
+
+export default Section
